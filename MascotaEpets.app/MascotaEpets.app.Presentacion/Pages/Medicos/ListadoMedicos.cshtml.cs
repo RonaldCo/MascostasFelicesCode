663cf1cd3e6d152baaf442dadb013284fc8f0d1b
Ronald Cortes <roco272012@hotmail.com>
2021-10-17 22:33:50 -0500

    avances del  proyecto

--- MascotaEpets.app/MascotaEpets.app.Presentacion/Pages/Medicos/ListadoMedicos.cshtml.cs
+++ MascotaEpets.app/MascotaEpets.app.Presentacion/Pages/Medicos/ListadoMedicos.cshtml.cs
@@ -4,13 +4,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MascotaEpets.app.Persistencia.AppRepositorios;
+using MascotaEpets.app.Dominio;
 
 namespace MascotaEpets.app.Presentacion.Pages
 {
     public class ListadoMedicosModel : PageModel
     {
+        //private readonly IRepositorioMedico repositorio;
+        //public IEnumerable<MascotaEpets.app.Dominio.MedicoVeterinario> medico=new IEnumerable<MascotaEpets.app.Dominio.MedicoVeterinario>;
         public void OnGet()
         {
+          //medico=repositorio.ConsultarMedicos().Tolist();
         }
+
+        /*public ListadoMedicosModel(IRepositorioMedico repositorio){
+
+            this.repositorio=repositorio;
+
+      }   */ 
+
     }
 }
+
--- MascotaEpets.app/MascotaEpets.app.Presentacion/Startup.cs
+++ MascotaEpets.app/MascotaEpets.app.Presentacion/Startup.cs
@@ -26,7 +26,7 @@ namespace MascotaEpets.app.Presentacion
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
-            services.AddSingleton<IRepositorioMedico,RepositorioMedico>();
+            //services.AddSingleton<IRepositorioMedico,RepositorioMedico>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
